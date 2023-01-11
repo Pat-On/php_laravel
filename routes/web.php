c854b121fcd053e96f25d71ff2cf2afb4f76824a
Patryk <patryk.r.nowak@gmail.com>
2023-01-11 21:44:42 +0000

    routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "About Page";
+});
+
+Route::get('/contact', function () {
+    return "Contact Page";
+});
+
+Route::get('/post/{id}', function($id){
+    return "$id";
+});
+
+Route::get('/post/{id}/name', function($id, $name){
+    return "$id $name";
+});
+
+// naming routes
+Route::get("admin/posts/example", array('as' => 'admin.home', function(){
+    $url = route('admin.home');
+    return $url;
+}));
