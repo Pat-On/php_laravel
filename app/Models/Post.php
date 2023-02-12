cda4186dd5d474381c7f42a4c2737cf20e29693a
Patryk <patryk.r.nowak@gmail.com>
2023-02-12 16:33:40 +0000

    mass asignment anmd hasmany relation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+
+    // Permission to mass assignment so basically it is like telling to the Laravel
+    // that is ok to save data to table
+    protected $fillable = [
+        'title',
+        'body'
+    ];
+
 }
