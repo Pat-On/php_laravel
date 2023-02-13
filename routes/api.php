587d8fcc85d68a2e289e55a273aff837b3ab4173
Patryk <patryk.r.nowak@gmail.com>
2023-02-13 09:11:44 +0000

    assigning and unassigning photo

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Photo;
 use App\Models\Staff;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -50,10 +51,34 @@ Route::get('/update/{id}', function ($id) {
     $photo->save();
 });
 
-
-Route::get('delete/{id}', function($id){
+Route::get('delete/{id}', function ($id) {
     $staff = Staff::findOrFail($id);
 
     // delete everything
     $staff->photos()->delete();
+});
+
+Route::get('/assign/{id}', function ($id) {
+    $staff = Staff::findOrFail($id);
+
+    $photo = Photo::findOrFail(11);
+
+    $staff->photos()->save($photo);
+});
+
+// idea:
+// - custom method to attach the photos
+
+Route::get('/unassign/{id}', function ($id) {
+    $staff = Staff::findOrFail($id);
+
+    // $photo = Photo::findOrFail(11);
+
+    // $staff->photos()->whereId(11)->update(['imageable_id' => null, 'imageable_type' => '']);
+    // $staff->photos()->whereId(11)->update(['imageable_id'=>null,'imageable_type'=>null]);
+
+    // funny db constrain - and how mysql engine is working
+    // in shortcut ' ' and null may be considered empty
+    // and engine is ignoring it and keeping old value
+    return $staff->photos()->whereId(11)->update(['imageable_id' => '0', 'imageable_type' => '0']);
 });
