fc870bcdff460dc870223ae74e5953be9b8190f6
Patryk <patryk.r.nowak@gmail.com>
2023-02-14 04:17:42 +0000

    end of polymorphic: many to many

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function tags(){
+        return $this->morphToMany('App\Models\Tag', 'taggable');
+    }
 }
