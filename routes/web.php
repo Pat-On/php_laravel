9f909db16d825b8ed9ab6e247113da14445c52d5
Patryk <patryk.r.nowak@gmail.com>
2023-02-17 13:14:45 +0000

    time in the laravel - carbon

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Carbon;
 
 // use App\Http\Controllers as C; https://www.udemy.com/course/php-with-laravel-for-beginners-become-a-master-in-laravel/learn/lecture/22307374#questions/14999442
 
@@ -20,4 +21,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware'=> 'web'], function(){
     Route::resource('/posts', PostController::class);
+
+    Route::get('/dates', function(){
+        // carbon
+
+        $date = new DateTime('+1 week');
+
+        echo $date->format('m-d-y');
+
+        echo '<br/>';
+
+        echo Carbon::now();
+        echo '<br/>';
+        echo Carbon::now()->addDays(10)->diffForHumans();
+        echo '<br/>';
+        echo Carbon::now()->subMonths(5)->diffForHumans();
+
+    });
 });
